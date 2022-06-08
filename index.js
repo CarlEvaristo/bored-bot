281eb8ecf811127bcc018e3971e9fd9a05f5e592
CarlEverest78 <thepythoneer@gmail.com>
2022-06-08 13:15:21 +0200

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const textContainerEl = document.getElementById("txt-container")
 const newActivityButton = document.getElementById("new-activity")
+const container = document.querySelector(".container")
 
 newActivityButton.addEventListener("click", (event) => {
     event.target.disabled = true
+    textContainerEl.classList.remove("text-animation")
+    container.classList.remove("container-animation")
     fetch("https://apis.scrimba.com/bored/api/activity")
         .then(response => response.json())
         .then(data => {
             textContainerEl.textContent = data.activity
             event.target.disabled = false
+            setTimeout(function() {
+              textContainerEl.classList.add("text-animation")}, 1000)
+            container.classList.add("container-animation")
         })
 
 })
